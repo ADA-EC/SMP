8a33d40e8457d77587d2cde41ffa0f2b900ae2dd
rafaelfpinho <rafaelfpinho@usp.br>
2021-05-27 18:39:31 -0300

    Added data menu files

---
+++ Interface/Menu Dados/menuDados.cpp
@@ -0,0 +1,15 @@
+#include "menuDados.h"
+#include "ui_menuDados.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
